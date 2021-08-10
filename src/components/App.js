e47f8fc463d7ae9ca6ed0a54d297667c24eacf03
SEOYUN KIM <nado0903@gmail.com>
2021-08-10 14:36:55 +0900

    feat: Log In

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
-import AppRouter from './Router';
+import AppRouter from 'components/Router';
+import React, { useState, useEffect } from 'react';
+import { authService } from 'fbase';
 
 function App() {
-  return <AppRouter />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    //onAuthStateChanged는 user의 로그인 상태 변화를 체크함 + firebase가 실행될 때도 / (user)을 callback
+    authService.onAuthStateChanged(user => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []); // 처음만 실행
+  return (
+    <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}</> //initialize되었으면 router 보여줌
+  );
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { authService } from 'fbase';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
+  const onChange = event => {
+    //한글자씩 추가
+    const {
+      //event.target.name/value
+      target: { name, value },
+    } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async event => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        //계정 생성
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        //로그인
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  //
+  const toggleAccount = () => setNewAccount(prev => !prev);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? 'Create Account' : 'Sign In'}
+        />
+        {error}
+      </form>
+      {/* 토글 식으로 누르면 바뀜 */}
+      <span onClick={toggleAccount}>
+        {newAccount ? 'Sign In' : 'Create Account'}
+      </span>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
+
 export default Auth;
